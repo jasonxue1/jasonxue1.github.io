23ad5ea05c28ad4a7eff32251f24d56003f66565
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2024-04-22 01:14:13 +0800

    Update aes.js

--- safe-data-transfer/aes.js
+++ safe-data-transfer/aes.js
@@ -1,23 +1,7 @@
-function encrypt() {
-    var userKey = document.getElementById('userKey').value;
-    var data = 'your-data-to-be-encrypted';
-    var encryptedData = aesEncrypt(userKey, data);
-    // Send encryptedData to server (use fetch or XMLHttpRequest)
-}
-
-function decrypt() {
-    var userKey = document.getElementById('userKey').value;
-    var encryptedData = 'encrypted-data-from-server';
-    var decryptedData = aesDecrypt(userKey, encryptedData);
-    document.getElementById('output').value = decryptedData;
-}
-
 function aesEncrypt(key, data) {
-    // Use 'key' to encrypt 'data' (client-side encryption)
-    // Return encrypted data
-}
-
-function aesDecrypt(key, encryptedData) {
-    // Use 'key' to decrypt 'encryptedData' (client-side decryption)
-    // Return decrypted data
+   // 在客户端使用用户输入的密钥对数据进行 AES 加密
+   // 返回加密后的数据
+   // 这里可以使用现成的 AES 加密库，例如 CryptoJS
+   const encryptedData = CryptoJS.AES.encrypt(data, key).toString();
+   return encryptedData;
 }
